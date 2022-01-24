4fd166578c55610e5d039eaefe5a3f0a0c1e388b
OMARRUIZRUIZ1M1IS <86338353+OMARRUIZRUIZ1M1IS@users.noreply.github.com>
2022-01-23 19:07:11 -0600

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtplaca = new System.Windows.Forms.TextBox();
+            this.txthorai = new System.Windows.Forms.TextBox();
+            this.txthoras = new System.Windows.Forms.TextBox();
+            this.lblfecha = new System.Windows.Forms.Label();
+            this.lblcosto = new System.Windows.Forms.Label();
+            this.lvRegistro = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnborrar = new System.Windows.Forms.Button();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Placa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(65, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control de reggistro de estacionamiento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Placa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(224, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Costo del dia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fecha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "hora inicio";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "hora salida";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(48, 159);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(90, 28);
+            this.btnRegistrar.TabIndex = 7;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtplaca
+            // 
+            this.txtplaca.Location = new System.Drawing.Point(99, 107);
+            this.txtplaca.Name = "txtplaca";
+            this.txtplaca.Size = new System.Drawing.Size(100, 20);
+            this.txtplaca.TabIndex = 8;
+            // 
+            // txthorai
+            // 
+            this.txthorai.Location = new System.Drawing.Point(114, 87);
+            this.txthorai.Name = "txthorai";
+            this.txthorai.Size = new System.Drawing.Size(100, 26);
+            this.txthorai.TabIndex = 9;
+            // 
+            // txthoras
+            // 
+            this.txthoras.Location = new System.Drawing.Point(114, 128);
+            this.txthoras.Name = "txthoras";
+            this.txthoras.Size = new System.Drawing.Size(100, 26);
+            this.txthoras.TabIndex = 10;
+            // 
+            // lblfecha
+            // 
+            this.lblfecha.AutoSize = true;
+            this.lblfecha.Location = new System.Drawing.Point(126, 47);
+            this.lblfecha.Name = "lblfecha";
+            this.lblfecha.Size = new System.Drawing.Size(51, 20);
+            this.lblfecha.TabIndex = 11;
+            this.lblfecha.Text = "label8";
+            // 
+            // lblcosto
+            // 
+            this.lblcosto.AutoSize = true;
+            this.lblcosto.Location = new System.Drawing.Point(260, 141);
+            this.lblcosto.Name = "lblcosto";
+            this.lblcosto.Size = new System.Drawing.Size(35, 13);
+            this.lblcosto.TabIndex = 12;
+            this.lblcosto.Text = "label9";
+            // 
+            // lvRegistro
+            // 
+            this.lvRegistro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvRegistro.HideSelection = false;
+            this.lvRegistro.Location = new System.Drawing.Point(12, 204);
+            this.lvRegistro.Name = "lvRegistro";
+            this.lvRegistro.Size = new System.Drawing.Size(559, 120);
+            this.lvRegistro.TabIndex = 13;
+            this.lvRegistro.UseCompatibleStateImageBehavior = false;
+            this.lvRegistro.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "fecha";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "hora inicio";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "hora fin";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "horas";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "tarifa";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "importe";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(58, 341);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(89, 32);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(171, 344);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 29);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblfecha);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txthorai);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txthoras);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(458, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 158);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fecha y Hora";
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Location = new System.Drawing.Point(351, 344);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(75, 23);
+            this.btnborrar.TabIndex = 17;
+            this.btnborrar.Text = "borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 385);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lvRegistro);
+            this.Controls.Add(this.lblcosto);
+            this.Controls.Add(this.txtplaca);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtplaca;
+        private System.Windows.Forms.TextBox txthorai;
+        private System.Windows.Forms.TextBox txthoras;
+        private System.Windows.Forms.Label lblfecha;
+        private System.Windows.Forms.Label lblcosto;
+        private System.Windows.Forms.ListView lvRegistro;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnborrar;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string dia;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblfecha.Text = DateTime.Now.ToShortDateString();
+
+            DateTime fecha = DateTime.Parse(lblfecha.Text);
+            dia = fecha.ToString("dddd");
+            MessageBox.Show(dia);
+
+            double costo = 0;
+            switch (dia)
+            {
+                case "domingo": costo = 2; break;
+                case "lunes": costo = 4; break;
+                case "martes": costo = 4; break;
+                case "miércoles": costo = 4; break;
+                case "jueves":costo = 4; break;
+                   
+
+                case "viernes":
+                case "sabado":
+                    costo = 7; break;
+            }
+            lblcosto.Text = costo.ToString("0.00");
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e) 
+        {
+            string placa = txtplaca.Text;
+            double costo = double.Parse(lblcosto.Text);
+            DateTime fecha = DateTime.Parse(lblfecha.Text);
+            DateTime horainicio = DateTime.Parse(txthorai.Text);
+            DateTime horasalida = DateTime.Parse(txthoras.Text);
+
+            //calcular hora
+
+            TimeSpan hora = horasalida - horainicio;
+
+            // calcular nimporte
+
+            double importe = costo * (hora.TotalHours);
+
+
+            // pasar todo al lisviw
+
+            ListViewItem fila = new ListViewItem(placa);
+            fila.SubItems.Add(fecha.ToString("d"));
+            fila.SubItems.Add(horainicio.ToString("t"));
+            fila.SubItems.Add(horasalida.ToString("t"));
+
+            fila.SubItems.Add(hora.TotalHours.ToString());
+            fila.SubItems.Add(costo.ToString("c"));
+            fila.SubItems.Add(importe.ToString("c"));
+            lvRegistro.Items.Add(fila);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtplaca.Clear();
+            txthoras.Clear();
+            txthorai.Clear();
+            txtplaca.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult R = MessageBox.Show("estas seguro que quieres salir", "señ@r",
+            MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+
+            if (R == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem fila in lvRegistro.SelectedItems)
+            {
+                fila.Remove();
+            }
+        }
+    }
+}
